14e3690e97c6a0a3723ef2651f8ca3686e45f5eb
Andy Nortrup <andrew.nortrup@gmail.com>
2016-06-19 22:09:38 -0700

    Split app and TA, figured out some of the OAuth2 flow

---
+++ TA-GoogleFitness/test/test_GoogleFitness.py
@@ -0,0 +1,7 @@
+import unittest
+from unittest.mock import MagicMock
+
+class TestGoogleFitness(unittest.TestCase):
+    def test_stream_events():
+        fit = GoogleFitness()
+        assert ew is splunklib.modularinput.EventWriter
