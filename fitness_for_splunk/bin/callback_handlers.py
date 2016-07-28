40dcfae1537e7f8725bceac630dcfb5224f07107
Andy Huynh <andyhuynh23@gmail.com>
2016-07-28 00:32:09 -0400

    Able to retrieve sessionKey, but password store not working.

--- fitness_for_splunk/bin/callback_handlers.py
+++ fitness_for_splunk/bin/callback_handlers.py
@@ -8,6 +8,10 @@ import splunk.bundle as bundle
 import httplib2, urllib, os, time
 import base64
 import json
+import xml.etree.ElementTree as ET
+import splunk.entity as entity
+
+#from test_modules import test_module
 
 """
 NOTE: To use this script, the following Python library dependencies need 
@@ -45,68 +49,110 @@ class fitbit_callback(splunk.rest.BaseRestHandler):
         Parse authorization code from URL query string
         """
         # Check for existence of 'code' parameter
-        for key in self.request['query']:
-            if key == 'code':
-                authCode = self.request['query']['code']
-            else:
-                authCode = None
+        #for key in self.request['query']:
+        #    if key == 'code':
+        #        authCode = self.request['query']['code']
+        #    else:
+        #        authCode = None
                 
-        if authCode is None:
-            self.response.setStatus(400)
-            self.response.setHeader('content-type', 'text/html')
-            self.response.write("Bad request: no authorization code")
-        else:
+        #if authCode is None:
+        #    self.response.setStatus(400)
+        #    self.response.setHeader('content-type', 'text/html')
+        #    self.response.write("Bad request: no authorization code")
+        #else:
             # TODO: Pull Client ID and Secret from password store
-            fitbit_secret_filepath = "C:\\Program Files\\Splunk\\etc\\apps\\fitness_for_splunk\\bin\\fitbit_secret.json"
-            with open(fitbit_secret_filepath, 'r') as file:
-                fitbit_config = json.load(file)
-            clientId = fitbit_config['web']['client_id'].encode("utf-8")
-            clientSecret = fitbit_config['web']['client_secret'].encode("utf-8")
-            callback_url = fitbit_config['web']['redirect_uris'][0].encode("utf-8")
+        #    fitbit_secret_filepath = "C:\\Program Files\\Splunk\\etc\\apps\\fitness_for_splunk\\bin\\fitbit_secret.json"
+        #    with open(fitbit_secret_filepath, 'r') as file:
+        #        fitbit_config = json.load(file)
+        #    clientId = fitbit_config['web']['client_id'].encode("utf-8")
+        #    clientSecret = fitbit_config['web']['client_secret'].encode("utf-8")
+        #    callback_url = fitbit_config['web']['redirect_uris'][0].encode("utf-8")
             
-            http = httplib2.Http()
+        http = httplib2.Http(disable_ssl_certificate_validation=True)
             
-            """
-            Exchange authorization code for token
-            """
-            # Get OAuth2 token
-            token_url = fitbit_config['web']['token_uri'].encode("utf-8")
-            encoded_id_secret = base64.b64encode( (clientId + ':' + clientSecret) )
-            authHeader_value = ('Basic ' + encoded_id_secret)
-            headers = {'Authorization': authHeader_value, 'Content-Type': 'application/x-www-form-urlencoded'}
-            data = {'clientId': clientId, 'grant_type': 'authorization_code', 'redirect_uri': callback_url, 'code': authCode}
-            body = urllib.urlencode(data)
+        """
+        Exchange authorization code for token
+        """
+        # Get OAuth2 token
+        #    token_url = fitbit_config['web']['token_uri'].encode("utf-8")
+        #    encoded_id_secret = base64.b64encode( (clientId + ':' + clientSecret) )
+        #    authHeader_value = ('Basic ' + encoded_id_secret)
+        #    headers = {'Authorization': authHeader_value, 'Content-Type': 'application/x-www-form-urlencoded'}
+        #    data = {'clientId': clientId, 'grant_type': 'authorization_code', 'redirect_uri': callback_url, 'code': authCode}
+        #    body = urllib.urlencode(data)
             
-            resp, content = http.request(token_url, 'POST', headers=headers, body=body)
+        #    resp, content = http.request(token_url, 'POST', headers=headers, body=body)
             
-            """
-            Use token to retrieve Full Name
-            """
-            token_json = json.loads(content)
-            user_id = token_json['user_id'].encode("utf-8")
-            access_token = token_json['access_token'].encode("utf-8")
+        """
+        Use token to retrieve Full Name
+        """
+        #    token_json = json.loads(content)
+        #    user_id = token_json['user_id'].encode("utf-8")
+        #    access_token = token_json['access_token'].encode("utf-8")
             
             # Get User Profile JSON
-            userProfile_url = 'https://api.fitbit.com/1/user/' + user_id + '/profile.json'
-            authHeader_value = ('Bearer ' + access_token)
-            headers = {'Authorization': authHeader_value, 'Content-Type': 'application/x-www-form-urlencoded'}
-            body = ""
-            resp, content = http.request(userProfile_url, 'GET', headers=headers, body=body)
+        #    userProfile_url = 'https://api.fitbit.com/1/user/' + user_id + '/profile.json'
+        #    authHeader_value = ('Bearer ' + access_token)
+        #    headers = {'Authorization': authHeader_value, 'Content-Type': 'application/x-www-form-urlencoded'}
+        #    body = ""
+        #    resp, content = http.request(userProfile_url, 'GET', headers=headers, body=body)
             
-            profile_json = json.loads(content)
-            full_name = profile_json['user']['fullName'].encode("utf-8")
-            username = user_id + "////" + full_name.replace(' ', '.')
-            realm = "fitbit"
+        #    profile_json = json.loads(content)
+        #    full_name = profile_json['user']['fullName'].encode("utf-8")
+        #    username = user_id + "////" + full_name.replace(' ', '.')
+        #    realm = "fitbit"
             
-            """
-            Store token in Splunk password store
-            """
-            # TODO
-            
-            self.response.setStatus(200)
-            #self.response.setHeader('content-type', 'text/html')
-            self.response.setHeader('content-type', 'application/json')
-            self.response.write(str(username))
+        """
+        Store token in Splunk password store
+        """
+        # TODO
+        
+        # Get a sessonKey to access password store
+        auth_url = "https://localhost:8089/services/auth/login"
+        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
+        data = {'username': 'admin', 'password': 'root'}
+        body = urllib.urlencode(data)
+        resp, content = http.request(auth_url, 'POST', headers=headers, body=body)
+
+        # Load XML
+        root = ET.fromstring(content)
+
+        # Parse XML and store sessionKey string
+        sessionKey = root.find('sessionKey').text
+
+        # Dummy username and token string to store
+        username = "FIEJWO8////Andy.Huynh"
+        token = "token"
+
+        """
+        # Sample code to retrieve password from Store
+        """
+        myapp = 'fitness_for_splunk'
+
+        try:
+            # list all credentials
+            entities = entity.getEntities(['admin', 'passwords'], namespace=myapp, owner='nobody', sessionKey=sessionKey) 
+        except Exception, e:
+            raise Exception("Could not get %s credentials from splunk. Error: %s" % (myapp, str(e)))
+
+        # return first set of credentials
+        #for i, c in entities.items(): 
+            #return c['username'], c['clear_password']
+
+        credentials = []
+
+        for i, c in entities.items():
+            # Append credentials to array
+            credentials.append((c['username'], c['clear_password']))
+
+        # Credentials array is blank when I visit the page
+        
+        
+        
+        self.response.setStatus(200)
+        #self.response.setHeader('content-type', 'text/html')
+        self.response.setHeader('content-type', 'application/json')
+        self.response.write(str(', '.join(credentials)))
             
     # listen to all verbs
     handle_POST = handle_DELETE = handle_PUT = handle_VIEW = handle_GET
