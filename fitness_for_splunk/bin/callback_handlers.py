4b92057f8d98ea9b5125b0e43f3ef8c6a679ed2b
Andy Huynh <andyhuynh23@gmail.com>
2016-07-30 18:43:16 -0400

    Fitbit REST endpoint stores tokens in KV Store. Added jsonbyteify.py module

--- fitness_for_splunk/bin/callback_handlers.py
+++ fitness_for_splunk/bin/callback_handlers.py
@@ -5,13 +5,19 @@
 from splunk import auth, search
 import splunk.rest 
 import splunk.bundle as bundle
+import splunk.entity as entity
+import splunklib.client as client
+
 import httplib2, urllib, os, time
 import base64
 import json
 import xml.etree.ElementTree as ET
-import splunk.entity as entity
 
-#from test_modules import test_module
+from oauth2client.client import flow_from_clientsecrets
+from apiclient.discovery import build
+
+import jsonbyteify
+#import splunkmethods
 
 """
 NOTE: To use this script, the following Python library dependencies need 
@@ -29,9 +35,8 @@ pyasn1
 pyasn1-modules
 rsa
 
+jsonbyteify
 """
-from oauth2client.client import flow_from_clientsecrets
-from apiclient.discovery import build
 
 # set our path to this particular application directory (which is suppose to be <appname>/bin)
 app_dir = os.path.dirname(os.path.abspath(__file__))
@@ -45,115 +50,93 @@ class fitbit_callback(splunk.rest.BaseRestHandler):
     Fitbit OAuth2 Callback Endpoint
     """
     def handle_GET(self):
-        """
-        Parse authorization code from URL query string
-        """
-        # Check for existence of 'code' parameter
-        #for key in self.request['query']:
-        #    if key == 'code':
-        #        authCode = self.request['query']['code']
-        #    else:
-        #        authCode = None
+        
+        # Parse authorization code from URL query string
+        authCode = None
+        errorCode = None
+        queryParams = self.request['query']
+        for param in queryParams:
+            if param == 'code':
+                authCode = queryParams['code']
+        
+        if authCode is None:
+            #TODO: Redirect to Error Authorizing Page
+            # No Authorization Code, return 400
+            self.response.setStatus(400)
+            self.response.setHeader('content-type', 'text/html')
+            self.response.write("Bad request: No authorization code")
+            return
+        
+        # Pull Client ID and Secret from password store
+        #TODO: Don't hardcode username and password
+        c = client.connect(host="localhost", port="8089", username="admin", password="root")
+        c.namespace.owner = "nobody"
+        #c.namespace.app = "fitness_for_splunk"
+        c.namespace.app = "TA-FitnessTrackers"
+        passwords = c.storage_passwords
                 
-        #if authCode is None:
-        #    self.response.setStatus(400)
-        #    self.response.setHeader('content-type', 'text/html')
-        #    self.response.write("Bad request: no authorization code")
-        #else:
-            # TODO: Pull Client ID and Secret from password store
-        #    fitbit_secret_filepath = "C:\\Program Files\\Splunk\\etc\\apps\\fitness_for_splunk\\bin\\fitbit_secret.json"
-        #    with open(fitbit_secret_filepath, 'r') as file:
-        #        fitbit_config = json.load(file)
-        #    clientId = fitbit_config['web']['client_id'].encode("utf-8")
-        #    clientSecret = fitbit_config['web']['client_secret'].encode("utf-8")
-        #    callback_url = fitbit_config['web']['redirect_uris'][0].encode("utf-8")
-            
-        http = httplib2.Http(disable_ssl_certificate_validation=True)
-            
-        """
-        Exchange authorization code for token
-        """
-        # Get OAuth2 token
-        #    token_url = fitbit_config['web']['token_uri'].encode("utf-8")
-        #    encoded_id_secret = base64.b64encode( (clientId + ':' + clientSecret) )
-        #    authHeader_value = ('Basic ' + encoded_id_secret)
-        #    headers = {'Authorization': authHeader_value, 'Content-Type': 'application/x-www-form-urlencoded'}
-        #    data = {'clientId': clientId, 'grant_type': 'authorization_code', 'redirect_uri': callback_url, 'code': authCode}
-        #    body = urllib.urlencode(data)
-            
-        #    resp, content = http.request(token_url, 'POST', headers=headers, body=body)
-            
-        """
-        Use token to retrieve Full Name
-        """
-        #    token_json = json.loads(content)
-        #    user_id = token_json['user_id'].encode("utf-8")
-        #    access_token = token_json['access_token'].encode("utf-8")
-            
-            # Get User Profile JSON
-        #    userProfile_url = 'https://api.fitbit.com/1/user/' + user_id + '/profile.json'
-        #    authHeader_value = ('Bearer ' + access_token)
-        #    headers = {'Authorization': authHeader_value, 'Content-Type': 'application/x-www-form-urlencoded'}
-        #    body = ""
-        #    resp, content = http.request(userProfile_url, 'GET', headers=headers, body=body)
-            
-        #    profile_json = json.loads(content)
-        #    full_name = profile_json['user']['fullName'].encode("utf-8")
-        #    username = user_id + "////" + full_name.replace(' ', '.')
-        #    realm = "fitbit"
-            
-        """
-        Store token in Splunk password store
-        """
-        # TODO
+        # Look for Client Secret
+        password = None
+        for entry in passwords:
+            if entry.realm == "fitbit":
+                password = entry
+                
+        if password is None:
+            #TODO: Redirect to Error Authorizing Page
+            # No password configured for Fitbit, return 500
+            self.response.setStatus(500)
+            self.response.setHeader('content-type', 'text/html')
+            self.response.write("Server Error: No password")
+            return
         
-        # Get a sessonKey to access password store
-        auth_url = "https://localhost:8089/services/auth/login"
-        headers = {'Content-Type': 'application/x-www-form-urlencoded'}
-        data = {'username': 'admin', 'password': 'root'}
+        # Exchange authorization code for OAuth2 token
+        http = httplib2.Http()
+        clientId = password.username
+        clientSecret = password.clear_password
+        callback_url = "https://localhost:8089/services/fitness_for_splunk/fitbit_callback"
+        auth_uri = "https://www.fitbit.com/oauth2/authorize"
+        token_url = "https://api.fitbit.com/oauth2/token"
+        
+        encoded_id_secret = base64.b64encode( (clientId + ':' + clientSecret) )
+        authHeader_value = ('Basic ' + encoded_id_secret)
+        headers = {'Authorization': authHeader_value, 'Content-Type': 'application/x-www-form-urlencoded'}
+        data = {'clientId': clientId, 'grant_type': 'authorization_code', 'redirect_uri': callback_url, 'code': authCode}
         body = urllib.urlencode(data)
-        resp, content = http.request(auth_url, 'POST', headers=headers, body=body)
-
-        # Load XML
-        root = ET.fromstring(content)
-
-        # Parse XML and store sessionKey string
-        sessionKey = root.find('sessionKey').text
-
-        # Dummy username and token string to store
-        username = "FIEJWO8////Andy.Huynh"
-        token = "token"
-
-        """
-        # Sample code to retrieve password from Store
-        """
-        myapp = 'fitness_for_splunk'
-
-        try:
-            # list all credentials
-            entities = entity.getEntities(['admin', 'passwords'], namespace=myapp, owner='nobody', sessionKey=sessionKey) 
-        except Exception, e:
-            raise Exception("Could not get %s credentials from splunk. Error: %s" % (myapp, str(e)))
-
-        # return first set of credentials
-        #for i, c in entities.items(): 
-            #return c['username'], c['clear_password']
-
-        credentials = []
-
-        for i, c in entities.items():
-            # Append credentials to array
-            credentials.append((c['username'], c['clear_password']))
-
-        # Credentials array is blank when I visit the page
+        resp, content = http.request(token_url, 'POST', headers=headers, body=body)
+        token_json = jsonbyteify.json_loads_byteified(content)
         
+        user_id = token_json['user_id']
+        access_token = token_json['access_token']
         
+        # Get User Profile JSON
+        userProfile_url = 'https://api.fitbit.com/1/user/' + user_id + '/profile.json'
+        authHeader_value = ('Bearer ' + access_token)
+        headers = {'Authorization': authHeader_value, 'Content-Type': 'application/x-www-form-urlencoded'}
+        body = ""
+        resp, content = http.request(userProfile_url, 'GET', headers=headers, body=body)
+        profile_json = jsonbyteify.json_loads_byteified(content)
+        full_name = profile_json['user']['fullName']
         
+        # Store id, name, and token in KV store
+        #TODO: Update Existing entries in KV Store
+        
+        collection_name = "fitbit_tokens"
+        if collection_name in c.kvstore:
+            c.kvstore.delete(collection_name)
+
+        # Let's create it and then make sure it exists    
+        c.kvstore.create(collection_name)
+        
+        kvstore = c.kvstore['fitbit_tokens']
+        kv_jsonstring = json.dumps({"id": user_id, "name": full_name, "token": token_json})
+        kvstore.data.insert(kv_jsonstring)
+        
+        #TODO: Redirect to Success Page
         self.response.setStatus(200)
-        #self.response.setHeader('content-type', 'text/html')
-        self.response.setHeader('content-type', 'application/json')
-        self.response.write(str(', '.join(credentials)))
-            
+        self.response.setHeader('content-type', 'text/html')
+        #self.response.setHeader('content-type', 'application/json')
+        self.response.write(str(kv_jsonstring))
+        
     # listen to all verbs
     handle_POST = handle_DELETE = handle_PUT = handle_VIEW = handle_GET
     
